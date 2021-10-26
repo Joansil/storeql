64698a869751745d7a4f3d42ad18908e597254d3
Joansil <joansilfs@gmail.com>
2021-10-26 11:49:03 -0300

    kafka cluster created

--- lib/storeql/choices/core/send_products_to_kafka.ex
+++ lib/storeql/choices/core/send_products_to_kafka.ex
@@ -6,6 +6,8 @@ defmodule Storeql.Choices.Core.SendProductsToKafka do
     hosts = [localhost: 9092]
     :ok = :brod.start_client(hosts, client_id)
     :ok = :brod.start_producer(client_id, @topic, [])
+
     :brod.produce(client_id, @topic, 0, _key = "", Jason.encode!(products))
+    # |> IO.inspect()
   end
 end
